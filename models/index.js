69c9a988f143a918a8a710e1bbc1c1678f41f879
Derrick Nguyen <ptkn2001@msn.com>
2021-10-08 23:24:11 -0700

    fixed foreign key issue

--- models/index.js
+++ models/index.js
@@ -3,16 +3,15 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
+// Categories have many Products
+Category.hasMany(Product, {
     foreignKey: 'category_id',
     onDelete: 'CASCADE',
 });
 
-// Categories have many Products
-Category.hasMany(Product, {
-    foreignKey: 'id',
-    onDelete: 'CASCADE',
+// Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
